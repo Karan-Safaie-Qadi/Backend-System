21523006f40d12b764e4a8e9ec46f3c9003ad9b2
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-22 22:06:56 +0330

    Add void return types to SessionTest

--- tests/SessionTest.php
+++ tests/SessionTest.php
@@ -9,33 +9,33 @@ use App\Core\Session;
 
 class SessionTest extends TestCase
 {
-    public function testSetAndGet()
+    public function testSetAndGet(): void
     {
         Session::set('test_key', 'test_value');
         $this->assertEquals('test_value', Session::get('test_key'));
     }
 
-    public function testHas()
+    public function testHas(): void
     {
         Session::set('exists_key', 'value');
         $this->assertTrue(Session::has('exists_key'));
         $this->assertFalse(Session::has('nonexistent'));
     }
 
-    public function testRemove()
+    public function testRemove(): void
     {
         Session::set('remove_key', 'value');
         Session::remove('remove_key');
         $this->assertFalse(Session::has('remove_key'));
     }
 
-    public function testGetDefault()
+    public function testGetDefault(): void
     {
         $this->assertNull(Session::get('nonexistent'));
         $this->assertEquals('default', Session::get('nonexistent', 'default'));
     }
 
-    public function testFlash()
+    public function testFlash(): void
     {
         Session::flash('flash_key', 'flash_value');
         $this->assertEquals('flash_value', Session::flash('flash_key'));
