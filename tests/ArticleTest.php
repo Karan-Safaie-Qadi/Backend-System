860439461cb0b6e67bb911c79003e6a609eea8dd
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-22 21:31:48 +0330

    test: add Article and ArticleSection unit tests

---
+++ tests/ArticleTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Article;
+use App\Models\ArticleSection;
+
+class ArticleTest extends TestCase
+{
+    public function testArticleExtendsModel()
+    {
+        $this->assertEquals('App\Core\Model', (new \ReflectionClass(Article::class))->getParentClass()->getName());
+    }
+
+    public function testArticleTable()
+    {
+        $r = new \ReflectionClass(Article::class);
+        $p = $r->getProperty('table');
+        $p->setAccessible(true);
+        $this->assertEquals('articles', $p->getValue(new Article()));
+    }
+
+    public function testArticleHasMethods()
+    {
+        foreach (['findBySlug', 'getByCategory', 'getPublished', 'getDrafts', 'publish', 'unpublish', 'getWithSections', 'getArchiveByMonth'] as $m) {
+            $this->assertTrue(method_exists(Article::class, $m), "Missing: $m");
+        }
+    }
+
+    public function testArticleSectionTypes()
+    {
+        $this->assertEquals('text', ArticleSection::TYPE_TEXT);
+        $this->assertEquals('list', ArticleSection::TYPE_LIST);
+        $this->assertEquals('table', ArticleSection::TYPE_TABLE);
+        $this->assertEquals('image', ArticleSection::TYPE_IMAGE);
+        $this->assertEquals('mixed', ArticleSection::TYPE_MIXED);
+    }
+
+    public function testArticleSectionHasMethods()
+    {
+        foreach (['getByArticle', 'addSection', 'getTableOfContents', 'reorder', 'deleteByArticle'] as $m) {
+            $this->assertTrue(method_exists(ArticleSection::class, $m), "Missing: $m");
+        }
+    }
+}
