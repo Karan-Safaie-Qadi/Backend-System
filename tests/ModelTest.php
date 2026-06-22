6a878bf073846ab6f8a7ee5ff6ccf90b240cf678
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-22 21:31:18 +0330

    test: add Model inheritance and method tests

---
+++ tests/ModelTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Core\Model;
+
+class ModelTest extends TestCase
+{
+    public function testModelIsAbstract()
+    {
+        $reflection = new \ReflectionClass(Model::class);
+        $this->assertTrue($reflection->isAbstract());
+    }
+
+    public function testModelExtendsBaseModel()
+    {
+        $reflection = new \ReflectionClass(Model::class);
+        $parent = $reflection->getParentClass();
+        $this->assertEquals('Models\Model', $parent->getName());
+    }
+
+    public function testModelHasRequiredMethods()
+    {
+        $methods = [
+            'find', 'all', 'create', 'updateRecord', 'deleteRecord',
+            'findBy', 'where', 'count', 'exists', 'paginate',
+            'search', 'pluck', 'latest', 'oldest',
+        ];
+        foreach ($methods as $method) {
+            $this->assertTrue(
+                method_exists(Model::class, $method),
+                "Method '$method' not found on Model"
+            );
+        }
+    }
+}
