3aac3f5b78293dbf6ac9e1d23f99f08dc369ba63
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-22 22:06:55 +0330

    Add void return types to MailerTest

--- tests/MailerTest.php
+++ tests/MailerTest.php
@@ -9,28 +9,28 @@ use App\Core\Mailer;
 
 class MailerTest extends TestCase
 {
-    public function testMailerClassExists()
+    public function testMailerClassExists(): void
     {
         $this->assertTrue(class_exists(Mailer::class));
     }
 
-    public function testMailerHasSendMethod()
+    public function testMailerHasSendMethod(): void
     {
         $this->assertTrue(method_exists(Mailer::class, 'send'));
     }
 
-    public function testMailerHasSendWithTemplateMethod()
+    public function testMailerHasSendWithTemplateMethod(): void
     {
         $this->assertTrue(method_exists(Mailer::class, 'sendWithTemplate'));
     }
 
-    public function testSendWithInvalidEmailThrows()
+    public function testSendWithInvalidEmailThrows(): void
     {
         $this->expectException(\RuntimeException::class);
         Mailer::send('', 'Test', 'Body');
     }
 
-    public function testSendWithTemplateInvalidTemplate()
+    public function testSendWithTemplateInvalidTemplate(): void
     {
         $this->expectException(\InvalidArgumentException::class);
         Mailer::sendWithTemplate('test@test.com', 'Test', 'nonexistent_template');
