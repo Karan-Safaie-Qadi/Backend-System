c3b7ce0bb432b38e16d1be8985750e56c213fb85
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-22 22:06:54 +0330

    Add void return types to ActivityLogTest

--- tests/ActivityLogTest.php
+++ tests/ActivityLogTest.php
@@ -9,12 +9,12 @@ use App\Models\ActivityLog;
 
 class ActivityLogTest extends TestCase
 {
-    public function testLogExtendsModel()
+    public function testLogExtendsModel(): void
     {
         $this->assertEquals('App\Core\Model', (new \ReflectionClass(ActivityLog::class))->getParentClass()->getName());
     }
 
-    public function testTimestampsDisabled()
+    public function testTimestampsDisabled(): void
     {
         $r = new \ReflectionClass(ActivityLog::class);
         $p = $r->getProperty('useTimestamps');
@@ -22,7 +22,7 @@ class ActivityLogTest extends TestCase
         $this->assertFalse($p->getValue(new ActivityLog()));
     }
 
-    public function testHasMethods()
+    public function testHasMethods(): void
     {
         foreach (['log', 'getByUser', 'getByEntity', 'getRecent', 'countToday', 'cleanOld'] as $m) {
             $this->assertTrue(method_exists(ActivityLog::class, $m), "Missing: $m");
