2f9bbb660c27565bc08833d23e2465f5d181d13c
Karan Safaie Qadi <karensefaie@gmail.com>
2026-06-22 21:31:48 +0330

    test: add Product model unit tests

---
+++ tests/ProductTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Product;
+
+class ProductTest extends TestCase
+{
+    public function testProductExtendsModel()
+    {
+        $this->assertEquals('App\Core\Model', (new \ReflectionClass(Product::class))->getParentClass()->getName());
+    }
+
+    public function testProductTable()
+    {
+        $r = new \ReflectionClass(Product::class);
+        $p = $r->getProperty('table');
+        $p->setAccessible(true);
+        $this->assertEquals('products', $p->getValue(new Product()));
+    }
+
+    public function testHasCoreMethods()
+    {
+        foreach (['findBySlug', 'findBySku', 'getByCategory', 'getActive', 'getFeatured', 'getOnSale', 'getLowStock', 'searchProducts', 'updateStock'] as $m) {
+            $this->assertTrue(method_exists(Product::class, $m), "Missing: $m");
+        }
+    }
+}
